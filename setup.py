121cc048002d0d1abe8fd97b02518d3a20c88521
Jack McVeigh <jmcveigh55@gmail.com>
2022-04-25 14:59:32 -0400

    Adding setup.py for installation

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='video-stepper',
+    version='0.1.0',
+    description=('CLI tool for creating frame by '
+                 'frame loops of your favorite videos'),
+    author='Jack McVeigh',
+    author_email='jmcveigh55@gmail.com',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['video-stepper=video_stepper.main:main'],
+    }
+)
